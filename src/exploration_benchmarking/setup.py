8c9a4b3e4e3a37aa5dba1a264825b2a0c4bf5e17
pierrebrd <pierrebrd7@gmail.com>
2025-06-12 10:30:29 +0200

    Add template for exploration_benchmarking, add stage_ros2 submodule

---
+++ src/exploration_benchmarking/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+package_name = "exploration_benchmarking"
+
+setup(
+    name=package_name,
+    version="0.0.1",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="pierrebrd",
+    maintainer_email="pierrebrd7@gmail.com",
+    description="Launch the benchmarking of an exploration algorithm",
+    license="MIT",
+    entry_points={
+        "console_scripts": [],
+    },
+)
