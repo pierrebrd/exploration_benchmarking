943c810c6d29fa15973f2d549c3914fc4274387f
pierrebrd <pierrebrd7@gmail.com>
2025-06-13 12:27:17 +0200

    Finished simple launch file, the package builds and seems to run correctly

--- src/exploration_benchmarking/launch/simple_simulation.launch.py
+++ src/exploration_benchmarking/launch/simple_simulation.launch.py
@@ -16,6 +16,7 @@ def generate_launch_description():
     # Create the arguments
     use_sim_time = LaunchConfiguration("use_sim_time")
     use_sim_time_arg = DeclareLaunchArgument("use_sim_time", default_value="true")
+
     nav2_params_file = LaunchConfiguration("nav2_params_file")
     nav2_params_file_arg = DeclareLaunchArgument(
         "nav2_params_file",
@@ -28,8 +29,26 @@ def generate_launch_description():
         ),
     )
 
+    exploration_params_file = LaunchConfiguration("exploration_params_file")
+    exploration_params_file_arg = DeclareLaunchArgument(
+        "exploration_params_file",
+        default_value=os.path.join(
+            FindPackageShare("explore_lite").find("explore_lite"),
+            "config",
+            "params.yaml",
+        ),
+    )
+
+    log_level = LaunchConfiguration("log_level")
+    log_level_arg = DeclareLaunchArgument(
+        "log_level",
+        default_value="info",
+        description="Log level for the nodes. To use debug without showing the rcl logs, use 'debug --log_level rcl:=warn'",
+    )
+    # TODO: add rviz parameters
+
     # Launch Stage
-    stage_ros2_share = FindPackageShare("stage_ros2")
+    stage_ros2_share = FindPackageShare("stage_ros2").find("stage_ros2")
     stage_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(stage_ros2_share, "launch", "stage.launch.py")
@@ -42,10 +61,26 @@ def generate_launch_description():
         }.items(),
     )
 
-    # Nav2 stack
+    # Launch the SLAM algorithm
+    # For now, we use the ros2 port of gmapping
+    slam_node = Node(
+        package="gmapper",
+        executable="gmap",
+        name="slam_gmapping",
+        output="screen",
+        parameters=[
+            {
+                "use_sim_time": use_sim_time,
+            }
+        ],
+        remappings=[("/scan", "/base_scan")],
+        arguments=["--ros-args", "--log-level", log_level],
+    )
+
+    # Launch Nav2 stack
     # We don't need the full Nav2 stack (map management, slam, etc.), just basic navigation capabilities
     # by listening to the map created by the SLAM algorithm and the goals/paths created by the exploration algorithm
-    nav2_bringup_share = FindPackageShare("nav2_bringup")
+    nav2_bringup_share = FindPackageShare("nav2_bringup").find("nav2_bringup")
     nav2_navigation_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(nav2_bringup_share, "launch", "navigation_launch.py")
@@ -55,8 +90,36 @@ def generate_launch_description():
             "namespace": "",
             "autostart": "true",
             "params_file": nav2_params_file,
+            "log_level": log_level,
         }.items(),
     )
 
+    # Launch the exploration algorithm
+    # Little test using explore_lite, but it would be better to use a custom launch file for the exploration algorithm and passing argumetns to it
+    # Explore-lite
+    explore_node = Node(
+        package="explore_lite",
+        name="explore_node",
+        executable="explore",
+        output="screen",
+        parameters=[
+            exploration_params_file,
+            {"use_sim_time": use_sim_time},
+        ],
+        remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
+        arguments=["--ros-args", "--log-level", log_level],
+    )
+
     # Return the LaunchDescription
-    return LaunchDescription([use_sim_time_arg, stage_launch])
+    return LaunchDescription(
+        [
+            use_sim_time_arg,
+            log_level_arg,
+            nav2_params_file_arg,
+            exploration_params_file_arg,
+            stage_launch,
+            slam_node,
+            nav2_navigation_launch,
+            explore_node,
+        ]
+    )
--- src/exploration_benchmarking/setup.py
+++ src/exploration_benchmarking/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = "exploration_benchmarking"
 
@@ -9,6 +11,11 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        (
+            "share/" + package_name + "/launch",
+            glob(os.path.join("launch", "*")),
+        ),
+        ("share/" + package_name + "/params", glob(os.path.join("params", "*"))),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
