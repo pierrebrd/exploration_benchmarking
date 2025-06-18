0e9fd6f53b397eb239a0c61a5dc1e8ec79545014
pierrebrd <pierrebrd7@gmail.com>
2025-06-18 10:52:06 +0200

    benchmark.py: handle /clock and /goal_sent

--- benchmark_utils/time_benchmark.py
+++ benchmark_utils/time_benchmark.py
@@ -1,3 +1,4 @@
+from pyexpat.errors import messages
 import sys
 import rclpy
 from rclpy.serialization import deserialize_message
@@ -6,6 +7,17 @@ from rosidl_runtime_py.utilities import get_message
 import yaml
 import os
 import tf2_msgs.msg
+
+## TODO : the use of global variables is not ideal
+
+## Global variables
+
+VERBOSE = False
+
+current_time = 0.0
+messages = []
+# List of goal, the time at which they were sent, and the time at which they were reached
+goals = []
 
 
 def read_rosbag2(file_path):
@@ -34,19 +46,44 @@ def read_rosbag2(file_path):
     del reader
 
 
+def process_messages(messages):
+    """Read the messages and select the appropriate action for each message"""
+
+    global current_time
+    global goals
+
+    for topic, data, timestamp in messages:
+        if topic == "/clock":
+            # Adjust current_time
+            current_time = data.clock.sec + data.clock.nanosec * 1e-9
+            if VERBOSE:
+                print(f"Current time updated: {current_time}")
+        if topic == "/goal_sent":
+            # Add the goal and the timestamp to the message.
+            goals.append([(data.x, data.y), current_time, None])
+            if VERBOSE:
+                print(f"Goal sent at {current_time}: {data.x}, {data.y}")
+
+
 def main():
+    global messages
+    global VERBOSE
+
     if len(sys.argv) < 2:
-        print("Usage: python time_benchmark.py <path_to_rosbag>")
+        print("Usage: python time_benchmark.py <path_to_rosbag> [--verbose]")
         sys.exit(1)
+
+    if len(sys.argv) > 2:
+        VERBOSE = sys.argv[2].lower() == "--verbose"
 
     file_path = sys.argv[1]
 
     messages = read_rosbag2(file_path)
+
+    process_messages(messages)
+
     # for topic, data, timestamp in messages:
-    #     print(data)
-    for topic, data, timestamp in messages:
-        if topic == "/clock":
-            print(f"Timestamp: {timestamp}, Topic: {topic}, Message: {data}")
+    #     print(f"Topic: {topic}, Timestamp: {timestamp}")
 
 
 if __name__ == "__main__":
