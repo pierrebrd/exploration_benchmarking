9c7f7ced46a8ce3301a6dec1048a5023bac338fc
pierrebrd <pierrebrd7@gmail.com>
2025-08-18 00:57:52 +0200

    Add odometry and estimated path to benchmark.py, iimprove the way the benchmark results are saved with a yaml file

--- benchmark_utils/benchmark.py
+++ benchmark_utils/benchmark.py
@@ -7,6 +7,12 @@ import subprocess
 import json
 import zipfile
 import argparse
+import yaml
+from tf2_msgs.msg import TFMessage
+from geometry_msgs.msg import TransformStamped
+from tf2_geometry_msgs import do_transform_pose_stamped, do_transform_pose
+from geometry_msgs.msg import PoseStamped, Pose
+from nav_msgs.msg import Odometry
 
 # Global variables
 
@@ -165,11 +171,20 @@ def process_messages(messages):
     # List of goal, the time at which they were sent, and the time at which they were reached
     goals = []
 
-    robot_path = []
+    ground_truth_path = []  # list containing (clock, x, y)
+    odometry_path = []  # list containing (clock, x, y)
+    distances = []  # list containing (clock, eucl., man_x, man_y, rot)
     current_time = 0.0  # Initialize current time
     start_time = None
     end_time = None
-    # previous_pose = None  # used to determine when the robot actually starts moving
+    # Then variables for estimated poses from tf
+    current_map_to_odom = None
+    current_odom_to_base_link = None
+    pose_initialized = False
+    initial_pose = None
+    current_pose = None
+    current_pose_tuple = None
+    estimated_path = []
 
     for topic, data, timestamp in messages:
         if topic == "/clock":
@@ -213,7 +228,7 @@ def process_messages(messages):
         elif topic == "/ground_truth":
             # We save the ground truth position of the robot
 
-            robot_path.append(
+            ground_truth_path.append(
                 (
                     data.header.stamp.sec + data.header.stamp.nanosec * 1e-9,
                     (data.pose.pose.position.x, data.pose.pose.position.y),
@@ -225,6 +240,78 @@ def process_messages(messages):
                     f"Ground truth position at {data.header.stamp.sec + data.header.stamp.nanosec * 1e-9}s: "
                     f"{data.pose.pose.position.x}, {data.pose.pose.position.y}"
                 )
+        elif topic == "/robot_distances":
+            # We keep track of the distance travled by the robot
+            distances.append(
+                (
+                    current_time,
+                    data.euclidean_distance,
+                    data.manhattan_distance_x,
+                    data.manhattan_distance_y,
+                    data.rotational_distance,
+                )
+            )
+        elif not pose_initialized and topic == "/odom":
+            # Initialize the pose from the first odom message
+            assert isinstance(data, Odometry)
+            initial_pose = data.pose.pose
+            pose_initialized = True
+            if verbose:
+                print(
+                    f"Initial pose set at {current_time}s: {initial_pose.position.x}, {initial_pose.position.y}"
+                )
+        elif topic == "/odom":
+            # We save the pure odometry
+            odometry_path.append(
+                (
+                    data.header.stamp.sec + data.header.stamp.nanosec * 1e-9,
+                    (data.pose.pose.position.x, data.pose.pose.position.y),
+                    (data.pose.pose.orientation.z, data.pose.pose.orientation.w),
+                )
+            )
+            if not pose_initialized:
+                # Initialize the pose from the first odom message
+                assert isinstance(data, Odometry)
+                initial_pose = data.pose.pose
+                pose_initialized = True
+                if verbose:
+                    print(
+                        f"Initial pose set at {current_time}s: {initial_pose.position.x}, {initial_pose.position.y}"
+                    )
+        elif topic == "/tf":
+            for transform in data.transforms:
+                # We only consider map->odom->base_link transform
+                if (
+                    transform.header.frame_id == "map"
+                    and transform.child_frame_id == "odom"
+                ):
+                    current_map_to_odom = transform
+                elif (
+                    transform.header.frame_id == "odom"
+                    and transform.child_frame_id == "base_link"
+                ):
+                    current_odom_to_base_link = transform
+                if (
+                    pose_initialized
+                    and current_map_to_odom
+                    and current_odom_to_base_link
+                ):
+                    current_pose = do_transform_pose(
+                        do_transform_pose(initial_pose, current_map_to_odom),
+                        current_odom_to_base_link,
+                    )
+                    current_pose_tuple = (
+                        current_time,
+                        (
+                            float(current_pose.position.x),
+                            float(current_pose.position.y),
+                        ),
+                        (
+                            float(current_pose.orientation.z),
+                            float(current_pose.orientation.w),
+                        ),
+                    )
+                    estimated_path.append(current_pose_tuple)
 
         # Additional method to check for exploration completion
         elif topic == "/rosout":
@@ -232,7 +319,15 @@ def process_messages(messages):
                 print(f"Exploration stopped at {current_time}s")
                 end_time = current_time
 
-    return (robot_path, goals, start_time, end_time)
+    return (
+        ground_truth_path,
+        odometry_path,
+        estimated_path,
+        distances,
+        goals,
+        start_time,
+        end_time,
+    )
 
 
 def main():
@@ -247,26 +342,141 @@ def main():
 
     messages = read_rosbag2(bag_path)
 
-    robot_path, goals, start_time, end_time = process_messages(messages)
+    results = {}  # Dictionary to store results
+    benchmark_path = os.path.join(run_path, "benchmark")
+    if not os.path.exists(benchmark_path):
+        os.makedirs(benchmark_path)
 
+    (
+        ground_truth_path,
+        odometry_path,
+        estimated_path,
+        distances,
+        goals,
+        start_time,
+        end_time,
+    ) = process_messages(messages)
+
+    # Save paths and distances to files
+
+    ground_truth_path_dicts = [
+        {
+            "time": t,
+            "position": {"x": pos[0], "y": pos[1]},
+            "orientation": {"z": orient[0], "w": orient[1]},
+        }
+        for t, pos, orient in ground_truth_path
+    ]
+
+    odometry_path_dicts = [
+        {
+            "time": t,
+            "position": {"x": pos[0], "y": pos[1]},
+            "orientation": {"z": orient[0], "w": orient[1]},
+        }
+        for t, pos, orient in odometry_path
+    ]
+
+    estimated_path_dicts = [
+        {
+            "time": t,
+            "position": {"x": pos[0], "y": pos[1]},
+            "orientation": {"z": orient[0], "w": orient[1]},
+        }
+        for t, pos, orient in estimated_path
+    ]
+
+    distances_dicts = [
+        {
+            "time": t,
+            "euclidean": eucl,
+            "manhattan_x": man_x,
+            "manhattan_y": man_y,
+            "rotational": rot,
+        }
+        for t, eucl, man_x, man_y, rot in distances
+    ]
+
+    ground_truth_path_file = os.path.join(benchmark_path, "ground_truth_path.yaml")
+    odometry_path_file = os.path.join(benchmark_path, "odometry_path.yaml")
+    estimated_path_file = os.path.join(benchmark_path, "estimated_path.yaml")
+    distances_file = os.path.join(benchmark_path, "distances.yaml")
+
+    with open(ground_truth_path_file, "w") as f:
+        yaml.dump(
+            ground_truth_path_dicts,
+            f,
+            default_flow_style=False,
+            indent=4,
+            sort_keys=False,
+        )
+
+    with open(odometry_path_file, "w") as f:
+        yaml.dump(
+            odometry_path_dicts,
+            f,
+            default_flow_style=False,
+            indent=4,
+            sort_keys=False,
+        )
+
+    with open(estimated_path_file, "w") as f:
+        yaml.dump(
+            estimated_path_dicts,
+            f,
+            default_flow_style=False,
+            indent=4,
+            sort_keys=False,
+        )
+
+    with open(distances_file, "w") as f:
+        yaml.dump(
+            distances_dicts, f, default_flow_style=False, indent=4, sort_keys=False
+        )
+
+    # Durations
+    results["start_time"] = start_time
+    results["end_time"] = end_time
     print(f"Exploration started at {start_time}s and ended at {end_time}s")
     if start_time and end_time:
+        results["duration"] = end_time - start_time
         print(f"total duration: {end_time - start_time}s")
 
+    # Final Distances
+    results["final_distances"] = {  # We save the last distance saved
+        "euclidean": distances[-1][1],
+        "manhattan_x": distances[-1][2],
+        "manhattan_y": distances[-1][3],
+        "rotational": distances[-1][4],
+    }
+    print(f"Final distances: {results['final_distances']}")
+
+    # Evo metrics
     ape_stats, rpe_stats = evo_metrics(
         run_path, "/ground_truth", "/tf:map.base_link", plot=True, align_origin=False
     )
     if ape_stats:
         print("APE stats:")
         print(ape_stats)
+        results["ape_stats"] = ape_stats
     else:
         print("No APE stats available or an error occurred.")
 
     if rpe_stats:
         print("RPE stats:")
         print(rpe_stats)
+        results["rpe_stats"] = rpe_stats
     else:
         print("No RPE stats available or an error occurred.")
+
+    # Progression over time
+    # We want to look at coverage over time
+
+    # Save results to YAML
+    yaml_path = os.path.join(benchmark_path, "benchmark.yaml")
+    with open(yaml_path, "w") as f:
+        yaml.dump(results, f, default_flow_style=False, indent=4, sort_keys=False)
+    print(f"Benchmark results saved to {yaml_path}")
 
 
 if __name__ == "__main__":
--- ros2_simulation/src/exploration_benchmarking/setup.py
+++ ros2_simulation/src/exploration_benchmarking/setup.py
@@ -11,10 +11,6 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        (
-            "share/" + package_name + "/launch",
-            glob(os.path.join("launch", "*")),
-        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
